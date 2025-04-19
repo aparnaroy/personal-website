1f48acf0f2a63a627f9de7e3e0e3bff689622f74
aparnaroy <aparnar@udel.edu>
2025-04-18 20:36:33 -0400

    Save current changes

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/aparnaroy/personal-website/app/layout":["static/media/a34f9d1faa5f3315-s.p.woff2","static/media/1b3800ed4c918892-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/aparnaroy/personal-website/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\",\"static/media/1b3800ed4c918892-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"pd3FHmWbcGE3cf6juzw5E3xRXZJx9Dk/e/6xJRfb8zw=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"dtLkvmETlNGFOFjEZKuBMeljrElq1cnK6mYozhyx0wA=\"\n}"
